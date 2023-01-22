8f7de51447d588c617b2c0e86a3206506d3b0544
Jay <jay.pena1224@proton.me>
2023-01-22 00:08:27 -0500

    Create comment to say why I used the methods I did

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(array, ...x) {
-    x.map(num => {
-        for(let i = 0; i < array.length; i++){
+    x.map(num => { // loop through rest parameters first using map. map is an iterative loop allows you to execute a function on the current item
+        for(let i = 0; i < array.length; i++){ // loop through array now to check for a match
             if (array[i] === num) {
                 array.splice(i, 1);
             }
