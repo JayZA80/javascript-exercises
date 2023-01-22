193b2372e7fcc2572fc36bc15faccb7715948f22
Jay <jay.pena1224@proton.me>
2023-01-22 01:31:58 -0500

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const roundToTenth = function(x) {
+  return Math.round(x * 10) / 10
+}
+
+const convertToCelsius = function(x) {
+  return roundToTenth((x - 32) * (5/9));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  return roundToTenth(x * 9/5 + 32);
 };
 
 // Do not edit below this line
