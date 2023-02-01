ca9a0ea7a5979384d556064fa6958e1af1b96ae7
Jay <jay.pena1224@proton.me>
2023-01-31 22:05:16 -0500

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  array.map(number => {
+    total += number
+  });
+  return number
 };
 
-const multiply = function() {
-
+const multiply = function(x, y) {
+  return x * y;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  let finalNumber = 0;
+	for (let i = 0; i <= y; i++) {
+    finalNumber += x * x;
+  }
+  return finalNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let nums = [x];
+  let currentValue = x;
+  while (currentValue > 1) {
+    nums.push(currentValue - 1);
+    currentValue--;
+  }
+  return nums.reduce((x, y) => {x * y});
 };
 
 // Do not edit below this line
