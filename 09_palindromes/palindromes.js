fa016e7727576517427b0eefc83531f0839a5fab
Jay <jay.pena1224@proton.me>
2023-01-31 23:05:43 -0500

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const alphaNum = (string) => {
+    return string.replace(/[^a-z0-9]/gi, '');
+}
 
+const palindromes = function (string) {
+    let string1 = alphaNum(string).toLowerCase();
+    let stringChars = Array.from(string1);
+    stringChars.reverse();
+    let string2 = alphaNum(stringChars.join()).toLowerCase();
+    if (string1 === string2) {
+        return true;
+    }
 };
 
 // Do not edit below this line
