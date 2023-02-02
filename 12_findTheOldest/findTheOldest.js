7e1716609f3c7ef831681c7193baed265328cc71
Jay <jay.pena1224@proton.me>
2023-02-02 15:00:10 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(list) {
+    let sortedList = list.sort(function(a, b) {
+        if (a.yearOfDeath === undefined) {
+          a.yearOfDeath = new Date().getFullYear();
+        } else if (b.yearOfDeath === undefined) {
+          b.yearOfDeath = new Date().getFullYear();
+        }
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        return lastGuy - nextGuy;
+      });
+    return sortedList[sortedList.length - 1];
 };
 
 // Do not edit below this line
