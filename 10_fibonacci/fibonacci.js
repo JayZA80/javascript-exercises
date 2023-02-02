b04d1bb0e5f6805f85cec89b05906ff301415f2e
Jay <jay.pena1224@proton.me>
2023-02-01 21:21:54 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let nums = [0, 1];
+    for (let i = 1; i < x; i++) {
+        let sum = nums[i] + nums[i - 1];
+        nums.push(sum);
+    }
+    return nums[x];
 };
 
 // Do not edit below this line
